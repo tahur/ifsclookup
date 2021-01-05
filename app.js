0e41fb6f1b9653eeee7446f5e94293c0ecb254c5
Tahur <mdtahur@gmail.com>
2021-01-05 22:32:17 +0530

    add services

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require('https');
 const ejs = require('ejs');
+const { text } = require("body-parser");
 
 const app = express();
 
@@ -25,39 +26,61 @@ app.post("/", function (req, res) {
 })
 
 
-app.get("/:postURL", function (req, res) {
+app.get(/${lookupcode}, function (req, res) {
     const requestedIfsc = req.params.postURL;
 
-    console.log(lookupCode)
+console.log(lookupCode)
 
-    let url = "https://ifsc.razorpay.com/" + requestedIfsc
+let url = "https://ifsc.razorpay.com/" + requestedIfsc
 
 
-    https.get(url, function (response) {
+https.get(url, function (response) {
 
-        if (response.statusCode == 404) {
-            res.render("error")
+    if (response.statusCode === 404) {
+        res.render("error")
+    }
+
+
+    response.on("data", function (data) {
+        const ifscInfo = JSON.parse(data);
+
+        const ifscCode = ifscInfo.IFSC
+
+
+
+        const service = ["UPI", "RTGS", "IMPS", "NEFT"];
+
+        let trueServices = [];
+
+        for (let i = 0; i < service.length; i++) {
+            console.log(i)
+
+            if (ifscInfo[service[i]] === true) {
+                console.log([service[i]])
+                trueServices.push[service[i]]
+            }
         }
 
 
-        response.on("data", function (data) {
-            const ifscInfo = JSON.parse(data);
+        console.log(trueServices)
 
-            const ifscCode = ifscInfo.IFSC
 
-            res.render("result", {
-                ifscCode: ifscCode,
-                bankName: ifscInfo.BANK,
-                micrCode: ifscInfo.MICR,
-                bankBranch: ifscInfo.BRANCH,
-                bankCity: ifscInfo.CITY,
-                bankAddress: ifscInfo.ADDRESS,
-                bankDist: ifscInfo.DISTRICT,
-                bankState: ifscInfo.STATE,
-            })
 
+
+        res.render("result", {
+            ifscCode: ifscCode,
+            bankName: ifscInfo.BANK,
+            micrCode: ifscInfo.MICR,
+            bankBranch: ifscInfo.BRANCH,
+            bankCity: ifscInfo.CITY,
+            bankAddress: ifscInfo.ADDRESS,
+            bankDist: ifscInfo.DISTRICT,
+            bankState: ifscInfo.STATE,
+            bankService: trueServices
         })
+
     })
+})
 
 
 })
