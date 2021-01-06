b7bb93d514ed1270bdc1329bff46f16f752e8cb3
Tahur <mdtahur@gmail.com>
2021-01-06 19:58:18 +0530

    services tag add

--- app.js
+++ app.js
@@ -26,61 +26,68 @@ app.post("/", function (req, res) {
 })
 
 
-app.get(/${lookupcode}, function (req, res) {
+app.get("/:postURL", function (req, res) {
+
     const requestedIfsc = req.params.postURL;
 
-console.log(lookupCode)
+    if (requestedIfsc === lookupCode) {
 
-let url = "https://ifsc.razorpay.com/" + requestedIfsc
+        console.log(lookupCode)
+
+        let url = "https://ifsc.razorpay.com/" + requestedIfsc
 
 
-https.get(url, function (response) {
+        https.get(url, function (response) {
 
-    if (response.statusCode === 404) {
-        res.render("error")
+            if (response.statusCode === 404) {
+                res.render("error")
+            }
+
+
+            response.on("data", function (data) {
+                const ifscInfo = JSON.parse(data);
+
+                const ifscCode = ifscInfo.IFSC
+
+
+
+                const service = ["UPI", "RTGS", "IMPS", "NEFT"];
+
+                let trueServices = [];
+
+                for (let i = 0; i < service.length; i++) {
+                    console.log(i)
+
+                    if (ifscInfo[service[i]] === true) {
+                        console.log([service[i]])
+                        trueServices.push([service[i]])
+                    }
+                }
+
+
+                console.log(trueServices)
+
+
+
+
+                res.render("result", {
+                    ifscCode: ifscCode,
+                    bankName: ifscInfo.BANK,
+                    micrCode: ifscInfo.MICR,
+                    bankBranch: ifscInfo.BRANCH,
+                    bankCity: ifscInfo.CITY,
+                    bankAddress: ifscInfo.ADDRESS,
+                    bankDist: ifscInfo.DISTRICT,
+                    bankState: ifscInfo.STATE,
+                    bankServices: trueServices
+                })
+
+            })
+        })
+
     }
 
 
-    response.on("data", function (data) {
-        const ifscInfo = JSON.parse(data);
-
-        const ifscCode = ifscInfo.IFSC
-
-
-
-        const service = ["UPI", "RTGS", "IMPS", "NEFT"];
-
-        let trueServices = [];
-
-        for (let i = 0; i < service.length; i++) {
-            console.log(i)
-
-            if (ifscInfo[service[i]] === true) {
-                console.log([service[i]])
-                trueServices.push[service[i]]
-            }
-        }
-
-
-        console.log(trueServices)
-
-
-
-
-        res.render("result", {
-            ifscCode: ifscCode,
-            bankName: ifscInfo.BANK,
-            micrCode: ifscInfo.MICR,
-            bankBranch: ifscInfo.BRANCH,
-            bankCity: ifscInfo.CITY,
-            bankAddress: ifscInfo.ADDRESS,
-            bankDist: ifscInfo.DISTRICT,
-            bankState: ifscInfo.STATE,
-            bankService: trueServices
-        })
-
-    })
-})
 
 
 })
